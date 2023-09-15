c9f7cbe4b6dbddbea4518bceeca7dac64d7a4d7f
Alex Laurie <Alex.R.Laurie@gmail.com>
2023-09-15 13:04:53 +1000

    Update laptop.sh

--- laptop.sh
+++ laptop.sh
@@ -41,7 +41,7 @@ nala update && nala install code -y
 # Install Microsoft Edge Stable
 echo "Installing Microsoft Edge Stable"
 curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /usr/share/keyrings/microsoft-edge.gpg >/dev/null
-echo "deb [arch=amd64] signed-by=/usr/share/keyrings/microsoft-edge.gpg https://packages.microsoft.com/repos/edge stable main" | tee /etc/apt/sources.list.d/microsoft-edge.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main" | tee /etc/apt/sources.list.d/microsoft-edge.list
 nala update && nala install microsoft-edge-stable -y
 
 # Install Google Chrome

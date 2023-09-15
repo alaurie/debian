91043c2f7afc3fbf31267b18261d3c23d9f28948
Alex Laurie <Alex.R.Laurie@gmail.com>
2023-09-15 13:07:43 +1000

    Update laptop.sh

--- laptop.sh
+++ laptop.sh
@@ -34,7 +34,7 @@ systemctl enable thermald
 
 # Install vscode
 echo "Installing VSCode"
-curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg >/dev/null
+curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /usr/share/keyrings/vscode.gpg >/dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main' | tee /etc/apt/sources.list.d/vscode.list
 nala update && nala install code -y
 

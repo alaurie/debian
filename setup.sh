6cd9fa42df0f6d3d236b6886ce7839845bae3b35
Alex Laurie <Alex.R.Laurie@gmail.com>
2024-11-01 00:54:20 +0000

    Update to latest version.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ fi
 
 echo "Setting up Debian..."
 
-echo "What is the username to configure?"
+echo "What is the username to use for configuration?"
 read -r name
 HOME="/home/$name"
 
@@ -24,14 +24,8 @@ echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free"
 # Add i386 architecture
 dpkg --add-architecture i386
 
-# Install nala
-apt update && apt install nala -y
-
-# Upgrade
-nala upgrade -y
-
 # Install utils
-nala install dirmngr ca-certificates software-properties-common apt-transport-https curl build-essential -y
+apt install dirmngr ca-certificates software-properties-common apt-transport-https curl build-essential -y
 
 # Determine if laptop
 chassis=$(dmidecode -s chassis-type)
@@ -39,15 +33,16 @@ chassis=$(dmidecode -s chassis-type)
 if [[ $chassis == 'Notebook' ]]; then
     echo "Laptop detected! Installing thermald and auto-cpufreq"
     # Install thermald
-    nala install thermald -y
+    apt install thermald -y
     systemctl enable thermald
 
     # Install auto-cpufreq
     mkdir -p /home/$name/Git/auto-cpufreq
-    git clone https://github.com/AdnanHodzic/auto-cpufreq.git /home/$name/Git/auto-cpufreq
+    git clone https://github.com/AdnanHodzic/auto-cpufreq.git /home/$name/Git/
+    chown -R "$name":"$name" "$HOME"/Git/auto-cpufreq
     sh /home/$name/Git/auto-cpufreq/auto-cpufreq-installer --install
     auto-cpufreq --install
-    chown -R "$name":"$name" "$HOME"/Git/auto-cpufreq
+    
 fi
 
 # vscode repo
@@ -66,45 +61,46 @@ curl -fSsl https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | t
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] https://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
 
 # Install packages
-nala update
-nala install google-chrome-stable microsoft-edge-stable code -y
+apt update
+apt install google-chrome-stable microsoft-edge-stable code -y
 
 # Install NVIDIA drivers
 echo "Installing NVIDIA drivers"
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/3bf863cc.pub | gpg --dearmor | tee /usr/share/keyrings/nvidia-drivers.gpg >/dev/null
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64 /" | tee /etc/apt/sources.list.d/nvidia-drivers.list
-nala update && nala install nvidia-driver -y
-nala install nvidia-driver-libs:i386 -y
+apt update && apt install nvidia-driver -y
+apt install nvidia-driver-libs:i386 -y
 
 # Install Insync
 echo "Installing Insync"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 echo "deb http://apt.insync.io/debian bookworm non-free contrib" | tee /etc/apt/sources.list.d/insync.list
-nala update && nala install insync -y
-nala update && nala install insync-dolphin -y
+apt update && apt install insync -y
+apt update && apt install insync-dolphin -y
 
 # Install Steam
 echo "Installing Steam"
-nala install steam-installer -y
+apt install steam-installer -y
 
-# Removing Firefox and libreoffice
-nala purge libreoffice* firefox* -y
-nala autoremove -y
+# Removing libreoffice
+apt purge libreoffice*
 
 # Pyenv
-nala install libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+apt install libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
 export PYENV_ROOT="/home/$name/.pyenv"
 curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >>"$HOME"/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>"$HOME"/.bashrc
-echo 'eval "$(pyenv init -)"' >>"$HOME"/.bashrc
+{
+  echo 'export PYENV_ROOT="$HOME/.pyenv"'
+  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
+  echo 'eval "$(pyenv init -)"'
+} >> "$HOME"/.bashrc 
 
 # Correct permissions on pyenv directory
 chown -R "$name":"$name" "$HOME"/.pyenv
 
 # Install fonts
-nala install fonts-firacode -y
+apt install fonts-firacode -y
 
 # Install Meslo Nerd Font
 echo "Installing Meslo Nerd Font"
@@ -112,15 +108,13 @@ mkdir -p "$HOME/.local/share/fonts"
 curl -fSsL 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf' -o "$HOME/.local/share/fonts/MesloLGS NF Regular.ttf"
 
 # Install Flatpak
-nala install flatpak plasma-discover-backend-flatpak -y
+apt install flatpak plasma-discover-backend-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install pipewire
-nala install pipewire-audio -y
+apt install pipewire-audio -y
 
 # Install firewalld and firewall-config
-nala install firewalld firewall-config -y
+apt install firewalld firewall-config -y
 
-echo "Done! Rebooting in 10 seconds..."
-sleep 10
-reboot
+echo "Done! Reboot to apply changes."
